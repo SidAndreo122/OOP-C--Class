78d7872474685945e5912394e9db79f127b83808
Sidney Andreano <sidney122@comcast.net>
2026-02-06 14:47:53 +0000

    attendance

---
+++ lesson_6/attend_vector.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+void rotateLeft(std::vector<char>& v, int k);
+int main(void) {
+std::vector<char> attend {'1', 'u', '3', '4', 'g', '6', 'r', 'j', 'y', 'j',
+'d', 'd', 'f', 'h', 's', 'd', 'f', '5', 'a', 'd', 'h', '6', 'j', 's', 'd', 'f',
+'j', '1', 'u', 'b', 'c', 'z', 'c', '6', 'q', 'o', '9', 's', 'j', 'd', 'f'};
+int n_rotations = pow((50 * 2 - 75 / 3 + 20 % 7 + 20 + 12 / 4), 3);
+rotateLeft(attend, n_rotations);
+for(int i = 0; i < 5; i++) {
+std::cout << attend.at(i);
+}
+std::cout << '\n';
+return 0;
+}
+void rotateLeft(std::vector<char>& v, int k) {
+    if (v.empty()) return;
+    
+    int n = v.size();
+    k = k % n;  
+    
+    if (k == 0) return;  // No rotation needed
+    
+    
+    std::vector<char> temp(n);
+    
+    for (int i = 0; i < n; i++) {
+        temp[i] = v[(i + k) % n];
+    }
+    
+    v = temp;
+}
